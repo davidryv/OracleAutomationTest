09f168e15a90ca073a6675ad320107773f9f12cf
David Rios Y Valles Rodriguez <David Rios Y Valles Rodriguez>
2017-06-23 20:24:47 -0500

    Complete Password Success

--- src/test/java/OracleAutomationTest/tests/TwitterExampleIT.java
+++ src/test/java/OracleAutomationTest/tests/TwitterExampleIT.java
@@ -16,7 +16,7 @@ public class TwitterExampleIT extends DriverBase{
 
 
     @Test
-    public void TwitterLogInExample() throws Exception {
+    public void TwitterLogInExampleFailed() throws Exception {
         // Create a new WebDriver instance
         // Notice that the remainder of the code relies on the interface,
         // not the implementation.
@@ -48,15 +48,62 @@ public class TwitterExampleIT extends DriverBase{
         // Check the title of the page
         System.out.println("Page title is: " + driver.getTitle());
 
-        // Google's search is rendered dynamically with JavaScript.
+        // Twitter webpage is rendered dynamically with JavaScript.
         // Wait for the page to load, timeout after 10 seconds
         (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver d) {
-                return d.getTitle().toLowerCase().startsWith("Login");
+                return d.getTitle().toLowerCase().startsWith("login");
             }
         });
 
-        // Should see: "cheese! - Google Search"
+        // Should see: "LogIn"
+        System.out.println("Page title is: " + driver.getTitle());
+    }
+
+
+
+    @Test
+    public void TwitterLogInExampleSuccess() throws Exception {
+        // Create a new WebDriver instance
+        // Notice that the remainder of the code relies on the interface,
+        // not the implementation.
+        WebDriver driver = getDriver();
+
+        // And now use this to visit twitter
+        driver.get("https://twitter.com/");
+        // Alternatively the same thing can be done like this
+        // driver.navigate().to("https://twitter.com/");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        // Find the text input element by its name
+
+        WebElement element = driver.findElement(By.id("signin-email"));
+        element.click();
+
+        // Enter username
+        element.sendKeys("davidryv");
+
+        //Find the password element by its name
+        WebElement element2 = driver.findElement(By.id("signin-password"));
+        element2.click();
+        // Enter password
+
+        element2.sendKeys("Wrong Password");
+
+        // Now submit the form. WebDriver will find the form for us from the element
+        element.submit();
+
+        // Check the title of the page
+        System.out.println("Page title is: " + driver.getTitle());
+
+        // Twitter webpage is rendered dynamically with JavaScript.
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("login");
+            }
+        });
+
+        // Should see: "Twitter Timeline"
         System.out.println("Page title is: " + driver.getTitle());
     }
 
