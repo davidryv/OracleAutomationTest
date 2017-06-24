91829ca6f6043a1863db33f4a8fa51cfd3cf177b
David Rios Y Valles Rodriguez <David Rios Y Valles Rodriguez>
2017-06-23 21:26:49 -0500

    Forgot Password added , REST API Progress

--- src/test/java/OracleAutomationTest/tests/TwitterExampleIT.java
+++ src/test/java/OracleAutomationTest/tests/TwitterExampleIT.java
@@ -107,5 +107,47 @@ public class TwitterExampleIT extends DriverBase{
         System.out.println("Page title is: " + driver.getTitle());
     }
 
+    @Test
+    public void TwitterLogInExampleForgot() throws Exception {
+        // Create a new WebDriver instance
+        // Notice that the remainder of the code relies on the interface,
+        // not the implementation.
+        WebDriver driver = getDriver();
+
+        // And now use this to visit twitter
+        driver.get("https://twitter.com/account/begin_password_reset");
+        // Alternatively the same thing can be done like this
+        // driver.navigate().to("https://twitter.com/");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        // Find the text input element by its name
+
+        WebElement element = driver.findElement(By.xpath("//input[@id='Form-textbox is-required is-validatedRemotely']"));
+
+
+        // Enter username
+        element.sendKeys("resonancia_mx");
+        // Summit user
+        element.submit();
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        //Summit method of recovery
+        element.submit();
+
+        // Check the title of the page
+        System.out.println("Page title is: " + driver.getTitle());
+
+        // Twitter webpage is rendered dynamically with JavaScript.
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("twitter");
+            }
+        });
+
+        // Should see: "Twitter Timeline"
+        System.out.println("Page title is: " + driver.getTitle());
+    }
+
 
 }
---
+++ src/test/java/OracleAutomationTest/tests/TwitterExampleRest.java
@@ -0,0 +1,20 @@
+package OracleAutomationTest.tests;
+
+/**
+ * Created by david on 23/06/2017.
+ */
+
+import org.testng.annotations.Test;
+
+public class TwitterExampleRest {
+
+
+
+    @Test
+    public void TwitterRestFail() throws Exception {
+
+
+
+    }
+
+}
